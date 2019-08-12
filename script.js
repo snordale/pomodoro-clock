c1a9773d29ce1cf964b49c9f73a79b4f025fdf61
Noah Talerman <noahtal@umich.edu>
2019-08-12 13:05:03 -0700

    added functionality to timers and added play functionality

--- script.js
+++ script.js
@@ -24,3 +24,90 @@ subIcons.forEach(function (icon){
     }
 });
 
+function padNum(num) {
+    let string = '';
+    if (num.toString().length < 2) {
+        string = `0${num.toString()}` 
+    } else {
+        string = num.toString();
+    }
+    return string;
+}
+
+function decrementTimer(element, timer) {
+    let hours = Number(timer.slice(0, 2));
+    let minutes = Number(timer.slice(3, 5));
+    let seconds = 60;
+    --minutes;
+    setInterval(function () {
+        if (seconds > 0) { 
+            --seconds;
+            let string = padNum(seconds);
+            timer = padNum(hours) + ':' + padNum(minutes) + ':' + string;
+            element.textContent = timer;
+        }
+    }, 10);
+}
+
+function incrementTimer(element, timer) {
+    let hours = Number(timer.slice(0, 2));
+    let minutes = Number(timer.slice(3, 5));
+    let seconds = 0;
+    setInterval(function () {
+        if (seconds < 59) { 
+            ++seconds;
+            let string = padNum(seconds);
+            timer = padNum(hours) + ':' + padNum(minutes) + ':' + string;
+            if (seconds === 59) {
+                timer = padNum(hours) + ':' + padNum(++minutes) + ':' + '00';
+            }
+            element.textContent = timer;
+        }
+    }, 10);
+}
+
+function stopTimer(element, timer) {
+    let hours = Number(timer.slice(0, 2));
+    let minutes = Number(timer.slice(3, 5));
+    let seconds = Number(timer.slice(6, 8));
+    timer = padNum(hours) + ':' + padNum(++minutes) + ':' + padNum(seconds);
+    element.textContent = timer;
+}
+
+function startTimer(element, timer) {
+    let hours = Number(timer.slice(0, 2));
+    let minutes = Number(timer.slice(3, 5));
+    let seconds = Number(timer.slice(6, 8));
+    let totalSeconds = (hours * 3600) + (minutes * 60) + seconds;
+    setInterval(function() {
+        --totalSeconds;
+        if (totalSeconds === 0) {
+            stopTimer(element, timer);
+        }
+        hours = Math.floor(totalSeconds / 3600);
+        let remainder = totalSeconds % 3600;
+        minutes = Math.floor(remainder / 60);
+        seconds = remainder % 60;
+        timer = padNum(hours) + ':' + padNum(++minutes) + ':' + padNum(seconds);
+        element.textContent = timer;
+    }, 1000);
+}
+
+let timers = document.querySelectorAll('.timer');
+
+subIcons.forEach(icon => icon.addEventListener('mousedown', function() {
+    let iconArray = Array.from(subIcons);
+    let idx = iconArray.indexOf(icon);
+    decrementTimer(timers[idx], timers[idx].textContent);
+}))
+
+addIcons.forEach(icon => icon.addEventListener('mousedown', function() {
+    let iconArray = Array.from(addIcons);
+    let idx = iconArray.indexOf(icon);
+    incrementTimer(timers[idx], timers[idx].textContent);
+}))
+
+let playBtn = document.querySelector('#play');
+playBtn.addEventListener('mousedown', function() {
+    startTimer(timers[0], timers[0].textContent)
+})
